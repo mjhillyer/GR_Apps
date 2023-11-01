dc652ec9ec10d0ac7a558495b7306159fb871368
mjhillyer <matthew.hillyer@gmail.com>
2023-11-01 16:24:19 -0500

    Add files via upload

---
+++ Watchtower/module_10.py
@@ -0,0 +1,26 @@
+import requests
+
+def create_dojo_product(prodname, proddesc, prodteam):
+    dojo_base_url = 'http://usmarprsec01:8080'
+    dojo_token = 'Token 01b67547c3585a602d24f1a2c2367b3635366b6a'
+
+    url = dojo_base_url + '/api/v2/products/'
+
+    payload = {'name':prodname,
+        'description':proddesc,
+        'lifecycle':'production',
+        'origin':'internal',
+        'prod_type':prodteam
+    }
+
+    headers = {
+        'Authorization':dojo_token  
+    }
+
+    webcall = requests.request("POST", url, headers=headers, data=payload)
+
+    results = webcall.json()
+
+    prodid = results["id"]
+
+    return prodid
