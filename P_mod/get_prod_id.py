e9c7cb0b6c599d0df7e54b683bcdb93ff9e93e91
mjhillyer <matthew.hillyer@gmail.com>
2023-11-01 16:35:00 -0500

    Update get_prod_id.py

--- P_mod/get_prod_id.py
+++ P_mod/get_prod_id.py
@@ -1,6 +1,24 @@
-def get_prod_id():
-    f = open("Unicorns.txt", "a")
-    f.write("This is a test")
-    f.close
-    return "success"
- 
+def create_dojo_product(prodname, proddesc, prodteam):
+    dojo_base_url = 'http://usmarprsec01:8080'
+    dojo_token = 'Token 01b67547c3585a602d24f1a2c2367b3635366b6a'
+
+    url = dojo_base_url + '/api/v2/products/'
+
+    payload = {'name':prodname,
+        'description':proddesc,
+        'lifecycle':'production',
+        'origin':'internal',
+        'prod_type':prodteam
+    }
+
+    headers = {
+        'Authorization':dojo_token  
+    }
+
+    webcall = requests.request("POST", url, headers=headers, data=payload)
+
+    results = webcall.json()
+
+    prodid = results["id"]
+
+    return prodid
