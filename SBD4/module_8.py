d4a9051bd910fdc8cf1e5757fff7f1ca2ebd4842
mjhillyer <matthew.hillyer@gmail.com>
2023-12-06 15:16:14 -0600

    Add files via upload

---
+++ SBD4/module_8.py
@@ -0,0 +1,26 @@
+def create_dojo_product(prodname, proddesc, prodteam):
+    import requests
+    
+    dojo_base_url = 'http://10.250.105.125:8080'
+    dojo_token = 'Token 01b67547c3585a602d24f1a2c2367b3635366b6a'
+
+    url = dojo_base_url + '/api/v2/products/'
+
+    payload = {'name':prodname,
+        'description':proddesc,
+        'lifecycle':'production',
+        'origin':'internal',
+        'prod_type':prodteam
+    }
+
+    headers = {
+        'Authorization':dojo_token  
+    }
+
+    webcall = requests.request("POST", url, headers=headers, data=payload)
+
+    results = webcall.json()
+
+    prodid = results["id"]
+
+    return prodid
